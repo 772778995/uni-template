2ac6ec913af3750a1f4a94d5b0236f9ec4dd404f
whc <772778995@qq.com>
2022-05-13 15:44:30 +0800

    取消将 api 挂载到 vue 实例

--- src/common/common.d.ts
+++ src/common/common.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 import _ from 'lodash'
-import $api from './api'
 import * as $uni from './uni'
 import $day from './day'
 import * as $util from './util'
@@ -9,7 +8,6 @@ declare module '@vue/runtime-core' {
   export interface ComponentCustomProperties {
     $_: typeof _
     $uni: typeof $uni
-    $api: typeof $api
     $day: typeof $day
     $util: typeof $util
   }
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,6 @@
 import { Plugin } from 'vue'
 import $_ from 'lodash'
 import * as $uni from './uni'
-import $api from './api'
 import $day from './day'
 import * as $util from './util'
 
@@ -10,7 +9,6 @@ export default {
     Object.assign(app.config.globalProperties, {
       $_,
       $uni,
-      $api,
       $day,
       $util
     })
