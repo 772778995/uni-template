0cb18cc4d3545d0cce8eb558a6a213f29dde9d79
whc <772778995@qq.com>
2022-05-13 16:07:38 +0800

    去除页面白名单校验

--- src/common/uni/interceptor.ts
+++ src/common/uni/interceptor.ts
@@ -1,19 +1,13 @@
-import { getStorageSync, showToast } from './index'
-
-const loginWhiteList = ['/pages/index/index']
+import { showModal } from '@/common/uni'
 
 const invoke = (arg: any) => {
-	arg.url = arg.url.replace(/@|(\.vue)/g, '')
-	if (process.env.NODE_ENV === 'production') {
-		if (loginWhiteList.includes(arg.url)) return true
-		return getStorageSync('userInfo') || showToast('页面不存在或无权访问')
-	}
+  arg.url = arg.url.replace(/@|(\.vue)/g, '')
 }
 
 const fail = (err: any) => {
-	const errMsg = err?.errMsg || '异常错误'
-	showToast(errMsg)
-	throw new Error(errMsg)
+  const content = err?.errMsg || '异常错误'
+  showModal({ title: '跳转页面异常', content })
+  throw new Error(content)
 }
 
 uni.addInterceptor('navigateTo', { invoke, fail })
