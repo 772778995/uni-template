1dcaa0eac82620e59a4ba4eeb1cceb074d5dd1e3
whc <772778995@qq.com>
2022-05-24 17:28:59 +0800

    配置 i18n

--- src/common/i18n/index.ts
+++ src/common/i18n/index.ts
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n'
-import zhCN from './langs/zh-CN'
-import en from './langs/en'
+import zhCN from './locales/zh-CN.json'
+import en from './locales/en.json'
 
 export default createI18n({
 	locale: 'zhCN',
--- src/common/i18n/langs/zh-CN.ts
+++
@@ -1,3 +0,0 @@
-export default {
-	title: 'uni 模板'
-}
