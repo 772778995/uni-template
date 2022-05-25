a1e1abd3d2e0231bc5cdb44ffcb4612c2b24539a
whc <772778995@qq.com>
2022-05-25 09:50:45 +0800

    修改 windicss 配置

--- windi.config.ts
+++ windi.config.ts
@@ -2,5 +2,17 @@ import { defineConfig } from 'windicss/helpers'
 import whcPreset from 'whc-windicss'
 
 export default defineConfig({
-  presets: [whcPreset()]
+	/* #ifdef MP-WEIXIN */
+	preflight: false,
+	/* #endif */
+	presets: [whcPreset({ mode: 'rpx' })],
+	theme: {
+		extend: {
+			height: {
+				'status-bar-height': 'var(--status-bar-height)',
+				'window-top': 'var(--window-top)',
+				'window-bottom': 'var(--window-bottom)'
+			}
+		}
+	}
 })
