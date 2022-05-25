355ce5674673c88177ffd9ec03aef9a0cbc13a70
whc <772778995@qq.com>
2022-05-25 10:43:41 +0800

    模板首页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,23 @@
 <template>
-  <view class="bg-black">1234</view>
+	<view class="flex flex-col h-full justify-around items-center">
+		<view class="text-xl">{{ $t('uni-template') }}</view>
+		<u-picker
+			v-model="pickerShow"
+			mode="selector"
+			:default-selector="[0]"
+			:range="$_.map(localeList, 'txt')"
+			@confirm="e => (locale = $_.map(localeList, 'locale')[e[0]])"
+		/>
+		<u-button @click="pickerShow = true">{{ $t('switch-lang') }}</u-button>
+	</view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-const title = ref('Hello')
+import { useI18n } from 'vue-i18n'
+import { localeList } from '@/common/i18n'
+import _ from 'lodash'
+
+const { locale } = useI18n()
+const pickerShow = ref(false)
 </script>
